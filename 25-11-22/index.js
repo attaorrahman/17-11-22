cb82c1315653f3a9f1f9d3811c06d8ab0b9ef39e
Atta Ur Rehman <malikattaurrahman@gmail.com>
2022-11-26 15:05:35 +0500

    All Done

--- 25-11-22/index.js
+++ 25-11-22/index.js
@@ -1,7 +1,13 @@
 let l1 = ['arham', 'huzaifa', 'aslam', 'asif']
 document.write(l1+ "<br>");
-let text;
+l2 = []
 for (let i = 0; i < l1.length; i++) {
-//   text += l1[i] + "<br>";
-  document.write(' Malik ' +l1[i] + "<br>");
+  l2.push(' Malik ' +l1[i]);
 }
+document.write(l2)
+
+// function Hello(){
+
+// document.write(l1+ "<br>");
+
+// }
