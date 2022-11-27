82e76421d610ba5d430d148a32c3315220d10695
Atta Ur Rehman <malikattaurrahman@gmail.com>
2022-11-27 13:50:27 +0500

    array objct

--- 25-11-22/index.js
+++ 25-11-22/index.js
@@ -1,13 +1,37 @@
-let l1 = ['arham', 'huzaifa', 'aslam', 'asif']
-document.write(l1+ "<br>");
-l2 = []
+let l1 = ["arham", "huzaifa", "aslam", "asif"];
+
+document.write(l1 + "<br>");
+
+l2 = [];
+
 for (let i = 0; i < l1.length; i++) {
-  l2.push(' Malik ' +l1[i]);
-}
-document.write(l2)
+  l2.push(" Malik " + l1[i]);
+};
 
-// function Hello(){
+function Hello() {
+  document.write(l2);
+};
 
-// document.write(l1+ "<br>");
+let students = [
+  {name: "Aslam", age: "29"},
+  {name: "Asad", age: "20"},
+  {name: "Ikram", age: "29"},
+  {name: "Uzair", age: "30"},
+  {name: "Khilji", age: "29"},
+  {name: "Usama", age: "17"},
+  {name: "Rehan", age: "18"},
+  {name: "Ahmad Shayan", age: "16"},
+  {name: "Asif Iqbal", age: "12"},
+]
+  // let userInp = prompt("Enter Student Name");
+  for(a=0;a<students.length;a++){
+    if(students[a].age <=18 ){
+      document.write(students[a].age+ (students[a].name));
+    }
+  }
 
-// }
+// ARRAY METHODS
+// Filter, 
+// let AllKids = [];
+
+// get all the students who have less then 18 age group.
